36cdf4fc259a4e7ba171a84b0ac9b0789aa53499
Michael <michael@mealsloth.com>
2016-08-21 18:22:41 -0700

    Workspace stuff and adding Chimera to installed apps

--- settings.py
+++ settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Siren',
+    'Chimera',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
